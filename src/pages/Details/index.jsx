896765c7ad65c1f5e1b3599a456d5424e277b34f
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-09 08:17:47 -0300

    propriedade

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -7,7 +7,9 @@ export function Details() {
     <Container>
       <h1>Hello World!</h1>
       <span>Junior Volonte</span>
-      <Button />
+      <Button title="Login" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
   );
 }
