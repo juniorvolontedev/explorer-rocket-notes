cea57cbbee924dd05083ad5011cd06a255413d56
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-09 08:06:54 -0300

    componente

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from "./styles.js";
 
+import { Button } from "../../components/Button/";
+
 export function Details() {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Junior Volonte</span>
+      <Button />
     </Container>
   );
 }
