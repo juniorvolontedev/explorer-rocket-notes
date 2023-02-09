b20190a9f44da0007eee74d47c3e3d48ed56e00e
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-09 18:27:57 -0300

    componente tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles.js";
 import { Header } from "../../components/Header/";
 import { Button } from "../../components/Button/";
 import { Section } from "../../components/Section/";
+import { Tag } from "../../components/Tag/";
 
 export function Details() {
   return (
@@ -18,6 +19,12 @@ export function Details() {
           </li>
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   );
