5c7515298a3a6a50c03c9cc0f7e8a9a8e455ed3f
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-09 17:00:23 -0300

    componente header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,12 @@
 import { Container } from "./styles.js";
 
+import { Header } from "../../components/Header/";
 import { Button } from "../../components/Button/";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Junior Volonte</span>
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
+      <Header />
       <Button title="Voltar" />
     </Container>
   );
