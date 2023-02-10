fba8775178afb6767b990934f68ff5e9a7fcb4c1
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-10 05:15:14 -0300

    formulario da signin

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,5 +1,25 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Signin() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
