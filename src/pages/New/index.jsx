e5780e1045f22f8c57ac30ed6f8084ebb1aef707
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-10 06:27:55 -0300

    finalizando o new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,9 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 import { Container, Form } from "./styles";
 
 export function New() {
@@ -17,6 +20,20 @@ export function New() {
           <Input placeholder="Título" />
 
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem placeholder="Novo link" isNew />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem placeholder="Nova tag" isNew />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
