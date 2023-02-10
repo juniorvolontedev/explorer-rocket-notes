f1a967a3ff667bad6d1607f6b4b1a926e4a2fc5a
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-10 05:53:34 -0300

    avatar do usuario

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
-import { Container, Form } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
@@ -13,6 +13,19 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img
+            src="https://github.com/juniorvolontedev.png"
+            alt="Foto do usuário"
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
         <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
