3401620ef2395ad45238417c1224ced9fa6477cb
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-09 21:52:26 -0300

    filtros

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return (
@@ -8,7 +9,17 @@ export function Home() {
         <h1>Rocketnotes</h1>
       </Brand>
       <Header />
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Node" />
+        </li>
+      </Menu>
       <Search></Search>
       <Content></Content>
       <NewNote></NewNote>
