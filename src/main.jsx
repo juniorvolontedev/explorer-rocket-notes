640f3d8f25e29ca896ab41a9f9ff30d50e3e95cb
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-10 05:32:06 -0300

    signup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
 
-import { Signin } from "./pages/Signin";
+import { SignUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Signin />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
