9c0a95d91ee9a1c2bfdc14cebebbc38f7192166b
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-15 08:02:07 -0300

    remover link da nota

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -17,6 +17,10 @@ export function New() {
     setNewLink("");
   }
 
+  function handleRemoveLink(deleted) {
+    setLinks((prevState) => prevState.filter((link) => link !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -33,7 +37,11 @@ export function New() {
 
           <Section title="Links úteis">
             {links.map((link, index) => (
-              <NoteItem key={String(index)} value={link} onClick={() => {}} />
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => handleRemoveLink(link)}
+              />
             ))}
             <NoteItem
               isNew
