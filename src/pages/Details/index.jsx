433c6d7b09f8624a9d4541cc24af1f850fb77cd6
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-15 21:32:42 -0300

    carregando detalhes da nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,48 +1,73 @@
+import { useEffect, useState } from "react";
 import { Container, Links, Content } from "./styles.js";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { Header } from "../../components/Header/";
 import { Section } from "../../components/Section/";
 import { Button } from "../../components/Button/";
 import { ButtonText } from "../../components/ButtonText/";
 import { Tag } from "../../components/Tag/";
+import { api } from "../../services/api.js";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const navigate = useNavigate();
+
+  const params = useParams();
+
+  function handleBack() {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  });
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
 
-          <h1>Introdução ao React</h1>
+            <h1>{data.title}</h1>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            delectus perspiciatis voluptates distinctio nihil! Magni nam
-            recusandae maxime natus deleniti, eveniet ratione quis pariatur at
-            aperiam obcaecati tempore cupiditate! Blanditiis?
-          </p>
+            <p>{data.description}</p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-            </Links>
-          </Section>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a target="_blank" href={link.url}>
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-          </Section>
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map((tag) => (
+                  <Tag key={tag.id} title={tag.name} />
+                ))}
+              </Section>
+            )}
 
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
